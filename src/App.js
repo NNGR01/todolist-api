b0f557561a19db298c7de529a9c026200e6a6039
Nicolas Guzman Rivera <guzman.r.nicolas@gmail.com>
2021-08-16 15:38:42 -0400

    trespuntotres

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>todo list - api</h1>
+      <h1>todo list - api </h1>
 <AddTask />
       </header>
   
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,119 +1,159 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const AddTask = () => {
-
-  const { task, setTask } = useState(null);
+  const [task, setTasks] = useState(null);
 
   useEffect(() => {
-
     const getTask = () => {
-      fetch('https://assets.breatheco.de/apis/fake/todos/user/NNGR',{
+      fetch("https://assets.breatheco.de/apis/fake/todos/user/fernandorp", {
         method: "GET",
         headers: {
-          "Content-Type": "application/json"
-        }
+          "Content-Type": "application/json",
+        },
       })
-      .then(resp => resp.json())
-      .then(data =>  data.msg ? createUser() : setTask(data))
-      .catch(error => console.log(error))
-}
-   getTask();
-},[])
+        .then((resp) => {
+          return resp.json();
+        })
+        .then((data) => {
+          if (data.msg) {
+            createUser();
+          } else {
+            setTasks(data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getTask();
+  }, []);
 
-useEffect( () => {
+  useEffect(() => {
+    const addTask = () => {
+      fetch("https://assets.breatheco.de/apis/fake/todos/user/fernandorp", {
+        method: "PUT",
+        body: JSON.stringify(task),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => {
+          console.log(resp.ok);
+          console.log(resp.status);
+          console.log(resp.text());
+          return resp.json();
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    addTask();
+  }, [task]);
 
-  const putTask = () =>{
-    fetch('https://assets.breatheco.de/apis/fake/todos/user/NNGR',{
-      method: 'PUT',
-      body: JSON.stringify(task),
-      headers:{
-        "Content-Type": "application/json"
-      }
+  const createUser = () => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/fernandorp", {
+      method: "POST",
+      body: JSON.stringify([]),
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    .then( resp => resp.json())
-    .then(data => console.log(data))
-    .catch( error => console.log(error))
-  }
-  putTask();
-},[task])
- 
-const createUser = () => {
-  fetch('https://assets.breatheco.de/apis/fake/todos/user/NNGR',{
-    method: 'POST',
-    body: JSON.stringify([]),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-  .then( resp => resp.json())
-  .then(data => setTask(data))
-  .catch(error => console.log(error))
-}
+      .then((resp) => {
+        console.log(resp.ok);
+        console.log(resp.status);
+        console.log(resp.text());
+        return resp.json();
+      })
+      .then((data) => {
+        setTasks(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-function addTarea(e){
-  if(e.key === "Enter" && e.target.value !== "" && task !== ""){
-    let nT = [...task];
-    let nE = {
-      lable: e.target.value,
-      done: false
+  function addtarea(e) {
+    if (e.key === "Enter" && e.target.value !== "" && task !== "") {
+      let nT = [...task];
+      let nE = {
+        label: e.target.value,
+        done: false,
+      };
+      let nTasks = nT.concat(nE);
+      setTasks(nTasks);
+      e.target.value = "";
     }
-   let nTask = nT.concat(nE);
-       setTask(nTask);
-       e.target.value = "";
   }
-}
-function deleteTask(e){
-  let taskUpdate = [];
-  task.map((elem, index) => {
-    if(index !== e){
-      taskUpdate.push(elem)
-    }
-    return taskUpdate
-  })
-  setTask(taskUpdate)
-}
 
-function deleteAll(){
-  const deleteAllTask = () => {
-    fetch('https://assets.breatheco.de/apis/fake/todos/user/NNGR',{
-      method: 'DETELE',
-      headers:{
-        "Content-Type": "application/json"
+  function deleteTask(e) {
+    let taskUpdated = [];
+    task.map((elem, index) => {
+      if (index !== e) {
+        taskUpdated.push(elem);
       }
-    })
-    .then( resp => resp.json())
-    .then (data => console.log(data))
-    .catch(error => console.log(error))
+      return taskUpdated;
+    });
+
+    setTasks(taskUpdated);
   }
-  deleteAllTask();
-  setTask([])
-}
 
+  function deleteAll() {
+    const deleteAllTasks = () => {
+      fetch("https://assets.breatheco.de/apis/fake/todos/user/fernandorp", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => {
+          console.log(resp.ok);
+          console.log(resp.status);
+          console.log(resp.text());
+          return resp.json();
+        })
+        .then((data) => {
+          console.log(data);
+          //SetTasks([])
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    deleteAllTasks();
+    setTasks([]);
+  }
 
-return (
-
-<div className="row">
-<div className="input-group mb-3">
-  <input type="text" className="form-control"
-  onKeyUp={addTarea}
-  />
-  <ul className="list-group">
-  {!!task &&
-                            task.map((elem, index) => {
-                                return (<div className="row" id={index} key={index}>
-                                    <div className="col-11" >{elem.label}</div>
-                                    <div className="col-1"><span className=" far fa-trash-alt" onClick={()=> deleteTask(index)}></span></div>
-                                </div>
-                                )
-                            })
-                        }
-  </ul>
-</div>
-  <button className="btn-warning col-md-1 mx-auto pt-2"></button>
-</div>
-
-)
-
-}
+  return (
+    <div className="row">
+      <div className="input-group input-group-lg">
+        <input type="text" className="form-control" onKeyUp={addtarea} />
+        <ul className="list-group">
+          {!!task &&
+            task.map((elem, index) => {
+              return (
+                <div className="row list-group-item d-inline-flex align-items-center" id={index} key={index}>
+                  <div className="col-9">{elem.label}</div>
+                  <div className="col-1 btn">
+                    <span
+                    type="button"
+                      className="btn btn-danger  text-end"
+                      onClick={() => deleteTask(index)}
+                    ></span>
+                  </div>
+                </div>
+              );
+            })}
+        </ul>
+      </div>
+      <button
+        className="btn-warning col-md-1 mx-auto pt-2"
+        onClick={deleteAll}
+      ></button>
+    </div>
+  );
+};
 
 export default AddTask;
